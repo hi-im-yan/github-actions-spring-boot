de96be0f631225f6dd4be266b60b9e924b879942
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-09-13 10:28:30 -0400

    test: some test

--- src/test/java/com/yanajiki/githubactions/GithubActionsApplicationTests.java
+++ src/test/java/com/yanajiki/githubactions/GithubActionsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.yanajiki.githubactions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class GithubActionsApplicationTests {
 
 	@Test
+	@DisplayName("Test just to show on log")
 	void contextLoads() {
+
+		assertTrue(true);
 	}
 
 }
