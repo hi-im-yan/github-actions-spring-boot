b5fb051bbbaa0a6bbc929c8b0d1d5188eb5e4135
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-09-14 14:48:14 -0400

    test: fix test

--- src/test/java/com/yanajiki/githubactions/GithubActionsApplicationTests.java
+++ src/test/java/com/yanajiki/githubactions/GithubActionsApplicationTests.java
@@ -3,10 +3,12 @@ package com.yanajiki.githubactions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class GithubActionsApplicationTests {
 
 	@Test
