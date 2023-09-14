1f7a2ea4591f19fd295e444b4562623ef0196c26
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-09-14 14:32:20 -0400

    feat: access environment variables

--- src/main/java/com/yanajiki/githubactions/Controller.java
+++ src/main/java/com/yanajiki/githubactions/Controller.java
@@ -1,13 +1,17 @@
 package com.yanajiki.githubactions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Value("${what_env_is_this}")
+    private String whatEnvIsThis;
+
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm healthy";
+        return "I'm healthy. I'm on the environment: " + whatEnvIsThis;
     }
 }
